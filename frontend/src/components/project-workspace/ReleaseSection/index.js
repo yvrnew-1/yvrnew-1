82f7c814300323c2b6bce22af2642cde60bc7845
s01 <s0740818.8@gmail.com>
2025-08-06 11:02:45 +0200

    zip file creted adn updaet in db but it not actual zip file . their is bug

---
+++ frontend/src/components/project-workspace/ReleaseSection/index.js
@@ -0,0 +1,7 @@
+export { default as DatasetStats } from './DatasetStats';
+export { default as ExportOptionsModal } from './ExportOptionsModal';
+export { default as ReleaseHistoryList } from './ReleaseHistoryList';
+export { default as ReleaseConfigPanel } from './releaseconfigpanel';
+export { default as TransformationCard } from './TransformationCard';
+export { default as TransformationModal } from './TransformationModal';
+export { default } from './ReleaseSection.jsx';
